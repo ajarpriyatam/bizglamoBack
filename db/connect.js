ee5f269c5807bbf9acb0911645fadd84fdfb423f
ajarpriyatam <ajar.priyatam.786@gmail.com>
2023-02-08 20:30:30 +0530

    changed database connection

--- db/connect.js
+++ db/connect.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
 
 const connect = () => {
-  const username = process.env.USER;
-  const password = process.env.PASSWORD;
+  // const username = process.env.USER;
+  const username = "bizglamo";
+  // const password = process.env.PASSWORD;
+  const password = "7q1RnfKLZQZwhxzY";
   const adminPassword = encodeURIComponent(password);
   const uri = `mongodb+srv://${username}:${adminPassword}@cluster0.8ryu92v.mongodb.net/?retryWrites=true&w=majority`;
   mongoose.connect(uri, {
