56abb32eaae4f96d0da4d3fb2c105415d3feedcc
ajarpriyatam <ajar.priyatam.786@gmail.com>
2023-02-26 16:28:13 +0530

    ksjdnj

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require("./app.js");
 const connect = require("./db/connect.js");
-const cors=require("cors");
+
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "db/config.env" });
 }
 
-app.use(cors());
+
 connect();
 const server = app.listen(process.env.PORT, () => { 
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
